4fb6c9372b53be356271d32fa938ea2f605d591c
Palukuru Charan Kumar Reddy <85541737+charankumar0002@users.noreply.github.com>
2025-06-26 11:09:17 +0530

    Add PropTypes for components

--- src/components/KPISummary.jsx
+++ src/components/KPISummary.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import PropTypes from "prop-types";
 import CountUp from 'react-countup';
 
 const KPISummary = ( data ) => {
@@ -41,6 +42,10 @@ const KPISummary = ( data ) => {
       </div>
     </div>
   );
+};
+
+KPISummary.propTypes = {
+  data: PropTypes.arrayOf(PropTypes.object).isRequired,
 };
 
 export default KPISummary;
